9e289a3fe93654ea09accda8165db5dae7c7d655
ddorado.baguena <d_de_dorado@hotmail.com>
2017-02-20 12:42:20 +0100

    Get y visualización de las publicaciones de cada publicador en la home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,11 +11,12 @@ import { ModalCreatePage } from '../modal-create/modal-create';
 
 //Services
 import { PublicadorService } from '../../providers/publicador/publicador';
+import { PublicacionService } from '../../providers/publicacion/publicacion';
 
 @Component( {
 	selector: 'page-home',
 	templateUrl: 'home.html',
-	providers: [PublicadorService]
+	providers: [PublicadorService, PublicacionService]
 } )
 export class HomePage {
 
@@ -25,7 +26,8 @@ export class HomePage {
 		public navCtrl: NavController,  
 		public actionSheetCtrl: ActionSheetController,
 		public modalCtrl: ModalController,
-		public publicadorSvc: PublicadorService
+		public publicadorSvc: PublicadorService, 
+		public publicacionSvc: PublicacionService, 
 	) {
 
 		this.initializePublicadores();
@@ -37,8 +39,29 @@ export class HomePage {
 			
 			this.publicadores = [];
 
+			//ARRAY DE OBJECTS TIPO PUBLICADOR
     		items.forEach( ( item ) => {
-    			
+
+    			//Array de publicaciones para un publicador
+				item.publications.forEach( ( publication ) => {
+					
+					//Get de todas las publicaciones
+					this.publicacionSvc.getPublicaciones().subscribe( publicaciones => {
+
+						let publi;
+
+						//Get de la sigla de la publicacion por la key de cada publicacion que tiene el publicador
+						publi = publicaciones.filter( function( _publicacion ) {
+							
+							if ( publication.key == _publicacion.$key ) {
+
+								publication.code = _publicacion.code;
+							}
+
+						} );
+					} ) ;
+				} );
+
 				this.publicadores.push( item );
     		} ) ;	
 
